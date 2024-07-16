89baa563a1e35a4917448499435b16f560d679e7
Bullets-jyr <jyr@bullets.co.kr>
2024-07-16 19:07:08 +0900

    autoDispose modifier 실습

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 
+String jsonUrl = 'https://jsonplaceholder.typicode.com/posts';
+
+final postFutureProvider = FutureProvider.autoDispose<String?>(
+  (ref) async {
+    ref.onDispose(() {
+      print('onDispose');
+    });
+    final response = await http.get(Uri.parse('${jsonUrl}/1'));
+    if (response.statusCode == 200) {
+      return response.body;
+    }
+  },
+);
+
 final tmpProvider = StateProvider.family(
   (ref, int arg) => arg + 0,
 );
@@ -42,30 +56,73 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Consumer(
-            builder: (BuildContext context, WidgetRef ref, Widget? child) {
-              final id = ref.watch(idStateProvider);
-              final post = ref.watch(postFutureProviderFamily(id));
-              return post.when(
-                data: (data) {
-                  return Text('${data}');
-                },
-                error: (error, trace) => Text('${error}'),
-                loading: () => CircularProgressIndicator(),
+          child: TextButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => SecondPage(),
+                ),
               );
             },
+            child: Text('다음 페이지'),
           ),
+          // child: Consumer(
+          //   builder: (BuildContext context, WidgetRef ref, Widget? child) {
+          //     final id = ref.watch(idStateProvider);
+          //     final post = ref.watch(postFutureProviderFamily(id));
+          //     return post.when(
+          //       data: (data) {
+          //         return Text('${data}');
+          //       },
+          //       error: (error, trace) => Text('${error}'),
+          //       loading: () => CircularProgressIndicator(),
+          //     );
+          //   },
+          // ),
         ),
         floatingActionButton: Consumer(
           builder: (context, ref, child) {
             return FloatingActionButton(
               onPressed: () {
-                ref.read(idStateProvider.notifier).update((state) => state += 1);
+                ref
+                    .read(idStateProvider.notifier)
+                    .update((state) => state += 1);
               },
               tooltip: 'Increment',
               child: const Icon(
                 Icons.add,
               ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatefulWidget {
+  const SecondPage({super.key});
+
+  @override
+  State<SecondPage> createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Consumer(
+          builder: (context, ref, child) {
+            final post = ref.watch(postFutureProvider);
+            return post.when(
+              data: (data) {
+                return Text('$data');
+              },
+              error: (e, t) {
+                return Text('$e');
+              },
+              loading: () => CircularProgressIndicator(),
             );
           },
         ),
